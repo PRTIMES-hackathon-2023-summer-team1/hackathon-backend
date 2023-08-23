dc80ef0a08a1bb9cf9fd4c36b68e3e87b1dbbc34
shima004 <yellow030408@gmail.com>
2023-08-23 15:00:51 +0900

    add: Add database models

---
+++ go/models/user_model.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type User struct {
+	UserID    string    `gorm:"primaryKey" json:"user_id"`
+	Email     string    `gorm:"unique, not null" json:"email"`
+	Password  string    `gorm:"not null" json:"password"`
+	IsAdmin   bool      `json:"is_admin"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
