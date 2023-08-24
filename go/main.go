dcfad20a4907b11ef688fcb7392561051aa61441
shima004 <yellow030408@gmail.com>
2023-08-24 06:36:20 +0000

    fix: dummy data timezone

--- go/main.go
+++ go/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/PRTIMES-hackathon-2023-summer-team1/hackathon-backend/config"
 	"github.com/PRTIMES-hackathon-2023-summer-team1/hackathon-backend/db"
 	"github.com/PRTIMES-hackathon-2023-summer-team1/hackathon-backend/models"
@@ -9,11 +11,13 @@ import (
 
 func main() {
 	appInfo := config.LoadConfig()
+	time.Local = time.FixedZone("Asia/Tokyo", 9*60*60)
 	repo, sqlDB := db.Connect(appInfo.PostgresInfo)
 	defer sqlDB.Close()
 	models.Drop(repo)
 	models.Migrate(repo)
 	models.InsertDummyData(repo)
+	
 
 	r := router.NewRouter(repo)
 	r.Run(":8080")
