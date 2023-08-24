7001047e7a7fcfca8ebf42288716f320d65cd0a4
shima004 <yellow030408@gmail.com>
2023-08-24 01:40:12 +0000

    remove: remove unused column

--- go/models/booking.go
+++ go/models/booking.go
@@ -9,10 +9,6 @@ type Booking struct {
 	UserID       string    `gorm:"not null" json:"user_id"`
 	TourID       string    `gorm:"not null" json:"tour_id"`
 	Participants int       `gorm:"not null" json:"participants"`
-	Men          int       `json:"men"`
-	Woman        int       `json:"woman"`
-	Adults       int       `json:"adults"`
-	Children     int       `json:"children"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
 }
