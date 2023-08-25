84fa3ff612c35f752162de15e90ab856f6d297a3
GoRuGoo <dachanyr8989@gmail.com>
2023-08-25 11:12:17 +0900

    wip: もしChatGPTを使う場合に戻せる用にするためのコミット

--- go/go.mod
+++ go/go.mod
@@ -3,6 +3,8 @@ module github.com/PRTIMES-hackathon-2023-summer-team1/hackathon-backend
 go 1.21.0
 
 require (
+	github.com/aws/aws-sdk-go v1.44.331
+	github.com/ayush6624/go-chatgpt v0.3.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.1
@@ -29,6 +31,7 @@ require (
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
--- go/models/tour.go
+++ go/models/tour.go
@@ -1,9 +1,14 @@
 package models
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
 	"math/rand"
 	"time"
 
+	"github.com/ayush6624/go-chatgpt"
 	"github.com/jaswdr/faker"
 )
 
@@ -23,6 +28,50 @@ type Tour struct {
 	UpdatedAt       time.Time `json:"updated_at"`
 }
 
+type openAPIRepository struct {
+	c   *chatgpt.Client
+	ctx context.Context
+}
+
+func newOpenAPIRepository(key string) (*openAPIRepository, error) {
+	c, err := chatgpt.NewClient(key)
+	if err != nil {
+		return nil, err
+	}
+	ctx := context.Background()
+
+	return &openAPIRepository{c: c, ctx: ctx}, nil
+}
+
+func (o openAPIRepository) generateTour() (string, string, string, error) {
+
+	type response struct {
+		name        string
+		description string
+		body        string
+	}
+
+	res, err := o.c.SimpleSend(o.ctx, fmt.Sprintf(`
+  日本国内の旅行プランを考えてください。必要な情報はタイトルを表すnameと短い説明文であるdescription、Markdown形式で旅行の特徴を記述したbodyです。
+  レスポンスする形式は以下のようにしてください。
+  ---------------------------------------------------------
+  {"name":"","description":"","body":""}
+  ---------------------------------------------------------
+  またこの形式以外の内容を出力した場合、あなたの過失によって無関係の人々に危害を加えてしまう可能性があります。`))
+	if err != nil {
+		return "", "", "", err
+	}
+
+	var r response
+	err = json.Unmarshal([]byte(res.Choices[0].Message.Content), &r)
+	if err != nil {
+		return "", "", "", err
+	}
+
+	return r.name, r.description, r.body, nil
+
+}
+
 var japanPlaceNames = []string{
 	"東京", "大阪", "京都", "札幌", "名古屋", "福岡", "広島", "仙台", "横浜", "神戸",
 	"沖縄", "横浜", "川崎", "岡山", "新潟", "金沢", "長野", "静岡", "鹿児島", "宮崎",
@@ -30,16 +79,36 @@ var japanPlaceNames = []string{
 	"埼玉", "群馬", "栃木", "茨城", "福島", "青森", "秋田", "山形", "岩手", "北海道",
 }
 
+func selectRandomPlacename() string {
+	firstName := japanPlaceNames[rand.Intn(len(japanPlaceNames))]
+	secondName := japanPlaceNames[rand.Intn(len(japanPlaceNames))]
+	fullName := firstName + " " + secondName
+	return fullName
+}
+
 func NewDummyTour(userID string, isVisible bool, faker *faker.Faker) *Tour {
 	jst, _ := time.LoadLocation("Asia/Tokyo")
 	firstDay := faker.Time().Time(time.Now().AddDate(0, 0, 1)).In(jst)
 	maxCapacity := faker.RandomDigitNotNull()
+
+	key := os.Getenv("OPENAPI_KEY")
+	aiRepository, err := newOpenAPIRepository(key)
+	if err != nil {
+		// この関数はマイグレーションでしか走らないので失敗したら異常終了にする
+		log.Fatal(err)
+	}
+
+	tourName, tourDescription, tourBody, err := aiRepository.generateTour()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return &Tour{
 		TourID:          faker.UUID().V4(),
 		UserID:          userID,
-		Name:            faker.Person().Name(),
-		Description:     faker.Lorem().Sentence(10),
-		Body:            selectRandomPlacename(),
+		Name:            tourName,
+		Description:     tourDescription,
+		Body:            tourBody,
 		Price:           faker.RandomDigitNotNull(),
 		FirstDay:        firstDay,
 		LastDay:         firstDay.AddDate(0, 0, faker.RandomDigitNotNull()),
@@ -47,11 +116,4 @@ func NewDummyTour(userID string, isVisible bool, faker *faker.Faker) *Tour {
 		CurrentCapacity: 0,
 		IsVisible:       isVisible,
 	}
-}
-
-func selectRandomPlacename() string {
-	firstName := japanPlaceNames[rand.Intn(len(japanPlaceNames))]
-	secondName := japanPlaceNames[rand.Intn(len(japanPlaceNames))]
-	fullName := firstName + " " + secondName
-	return fullName
 }
